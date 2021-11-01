a832229ae6950d019f5b7e81a71cfd109541f078
jyl7094 <jyl7094@gmail.com>
2021-11-01 02:12:25 -0400

    Add basic functionalities

--- pomodoro.py
+++ pomodoro.py
@@ -1,136 +1,184 @@
 import tkinter as tk
 
 
-class Option(tk.Frame):
+class OptionFrame(tk.Frame):
     def __init__(self, container):
         super().__init__(container)
+        self.option = 'p'
+        self.clicked = False
+        self.create_widgets()
 
-        self.selection = ('Pomodoro', 'Short Break', 'Long Break')
-        self.selected = self.selection[0]
+    def create_widgets(self):
+        self.p_btn = tk.Button(self, text='Pomodoro', state='active', pady=5, command=lambda: self.combined('p'))
+        self.sb_btn = tk.Button(self, text='Short Break', pady=5, command=lambda: self.combined('sb'))
+        self.lb_btn = tk.Button(self, text='Long Break', pady=5, command=lambda: self.combined('lb'))
+        
+        self.p_btn.grid(row=0, column=0)
+        self.sb_btn.grid(row=0, column=1)
+        self.lb_btn.grid(row=0, column=2)
 
-        self.pomodoro_btn = tk.Button(self, text=self.selection[0], state='active', pady=5, command=self.select_pomodoro)
-        self.short_break_btn = tk.Button(self, text=self.selection[1], pady=5, command=self.select_short_break)
-        self.long_break_btn = tk.Button(self, text=self.selection[2], pady=5, command=self.select_long_break)
+    def select_option(self, option):
+        if option == 'p':
+            self.p_btn.config(state='active')
+            self.sb_btn.config(state='normal')
+            self.lb_btn.config(state='normal')
+            self.option = 'p'
+        elif option == 'sb':
+            self.p_btn.config(state='normal')
+            self.sb_btn.config(state='active')
+            self.lb_btn.config(state='normal')
+            self.option = 'sb'
+        elif option == 'lb':
+            self.p_btn.config(state='normal')
+            self.sb_btn.config(state='normal')
+            self.lb_btn.config(state='active')
+            self.option = 'lb'
+    
+    def toggle_options(self, val:str):
+        if val == 'enable':
+                self.select_option(self.option)
+        elif val == 'disable':
+            self.p_btn.config(state='disabled')
+            self.sb_btn.config(state='disabled')
+            self.lb_btn.config(state='disabled')
 
-        self.pomodoro_btn.grid(row=0, column=0)
-        self.short_break_btn.grid(row=0, column=1)
-        self.long_break_btn.grid(row=0, column=2)
+    def combined(self, option):
+        self.select_option(option)
+        self.set_clicked(True)
 
-    def select_pomodoro(self):
-        self.pomodoro_btn.config(state='active')
-        self.short_break_btn.config(state='normal')
-        self.long_break_btn.config(state='normal')
-        self.selected = self.selection[0]
+    def get_option(self):
+        return self.option
+    
+    def set_option(self, val:str=''):
+        self.option = val
 
-    def select_short_break(self):
-        self.pomodoro_btn.config(state='normal')
-        self.short_break_btn.config(state='active')
-        self.long_break_btn.config(state='normal')
-        self.selected = self.selection[1]
+    def get_clicked(self):
+        return self.clicked
 
-    def select_long_break(self):
-        self.pomodoro_btn.config(state='normal')
-        self.short_break_btn.config(state='normal')
-        self.long_break_btn.config(state='active')
-        self.selected = self.selection[2]
-
-    def get_selected(self):
-        return self.selected
+    def set_clicked(self, val:bool):
+        self.clicked = val
 
 
-class Timer(tk.Frame):
+class TimerFrame(tk.Frame):
     def __init__(self, container):
         super().__init__(container)
+        self.MINUTE = 60000
+        self.time = 25 * self.MINUTE
+        self.create_widgets()
 
-        self.time = 25*60000
-
-        self.timer = tk.Label(self, text=self.display_time(self.time), font=('Helvetica', 120))
+    def create_widgets(self):
+        self.timer = tk.Label(self, text=self.time_to_string(self.time), font=('Helvetica', 120))
 
         self.timer.grid(row=0)
 
-    def display_time(self, time): # ms -> mm:ss
-        minutes, seconds = divmod(time / 1000, 60)
+    def time_to_string(self, time): # ms -> mm:ss
+        minutes, seconds = divmod(time/1000, 60)
         return '{:02}:{:02}'.format(int(minutes), int(seconds))
-
-    def update_time(self):
-        self.time -= 1000
-        self.timer.config(text=self.display_time(self.time))
-
+    
     def set_time(self, time):
-        self.time = time
-        self.timer.config(text=self.display_time(self.time))
+        self.time = time * self.MINUTE
+        self.timer.config(text=self.time_to_string(self.time))
+
+    def countdown(self):
+        self.time -= 1000
+        self.timer.config(text=self.time_to_string(self.time))
+
+    def get_time(self):
+        return self.time
 
 
-class Tracker(tk.Frame):
+
+# class InfoFrame(tk.Frame):
+#     def __init__(self, container):
+#         super().__init__(container)
+#         self.cycle = 1
+#         self.event = 'Pomodoro'
+#         self.create_widgets()
+
+#     def create_widgets(self):
+#         self.details = tk.Label(self, text=f'Cycle {self.cycle} - {self.event}')
+
+#         self.details.grid(row=0)
+    
+#     def inc_cycle(self):
+#         self.cycle += 1
+#         self.details.config(text=f'Cycle {self.cycle} - {self.event}')
+    
+#     def set_event(self, event):
+#         self.event = event
+#         self.details.config(text=f'Cycle {self.cycle} - {self.event}')
+
+
+class ControllerFrame(tk.Frame):
     def __init__(self, container):
-        super().__init__(container)
+        super().__init__(container)       
+        self.started = ''
+        self.create_widgets()
 
-        self.counter = 1
-        self.tracker = ('Pomodoro', 'Break')
-        self.tracker_val = 0
-        self.curr_tracker = self.tracker[self.tracker_val]
-
-        self.details = tk.Label(self, text=self.display_details())
-
-        self.details.grid(row=0)
-    
-    def display_details(self):
-        return f'#{self.counter} {self.curr_tracker}'
-    
-    def inc_counter(self):
-        self.counter += 1
-        self.details.config(text=self.display_details())
-    
-    def update_curr_tracker(self):
-        self.tracker_val = not self.tracker_val
-        self.curr_tracker = self.tracker[self.tracker_val]
-        self.details.config(text=self.display_details())
-
-
-class Controller(tk.Frame):
-    def __init__(self, container):
-        super().__init__(container)
-        
-        self.started = False
-
-        self.start_btn = tk.Button(self, text='Start', pady=5, command=self.start_timer)
-        self.pause_btn = tk.Button(self, text='Pause', state='disabled', pady=5, command=self.pause_timer)
+    def create_widgets(self):
+        self.start_btn = tk.Button(self, text='Start', pady=5, command=lambda: self.toggle_timer('start'))
+        self.pause_btn = tk.Button(self, text='Pause', state='disabled', pady=5, command=lambda: self.toggle_timer('pause'))
 
         self.start_btn.grid(row=0, column=0)
         self.pause_btn.grid(row=0, column=2)
 
-    def start_timer(self):
-        self.started = True
-        self.start_btn.config(state='disabled')
-        self.pause_btn.config(state='normal')
-
-    def pause_timer(self):
-        self.started = False
-        self.start_btn.config(state='normal')
-        self.pause_btn.config(state='disabled')
+    def toggle_timer(self, val:str):
+        if val == 'start':
+            self.started = 'start'
+            self.start_btn.config(state='disabled')
+            self.pause_btn.config(state='normal')
+        elif val == 'pause':
+            self.started = 'pause'
+            self.start_btn.config(state='normal')
+            self.pause_btn.config(state='disabled')
+    
+    def set_started(self, val=''):
+        self.started = val
 
     def get_started(self):
         return self.started
-
+    
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-
         self.setup()
-        self.opened = False
+        self.create_widgets()
+        self.after_id = None
+        self.running = False
+        self.update()
 
-        self.option = Option(self)
-        self.timer = Timer(self)
-        self.tracker = Tracker(self)
-        self.controller = Controller(self)
+    def update(self):
+        started = self.controller.get_started()
+        if self.timer.get_time() <= 0 and self.after_id != None:
+            self.after_cancel(self.after_id)
+        if started == 'start':
+            self.running = True
+            self.option.toggle_options('disable')
+            self.after_id = self.after(1000, self.countdown)
+            self.controller.set_started()
+        elif started == 'pause':
+            if self.after_id != None:
+                self.running = False
+                self.option.toggle_options('enable')
+                self.after_cancel(self.after_id)
+
+        if not self.running:
+            option = self.option.get_option()
+            self.timer.set_time(self.timer.get_time()/60000)
+            if option == 'p' and self.option.get_clicked():
+                self.timer.set_time(25)
+            elif option == 'sb' and self.option.get_clicked():
+                self.timer.set_time(5)
+            elif option == 'lb' and self.option.get_clicked():
+                self.timer.set_time(15)
+        self.option.set_clicked(False)
         
-        self.option.grid(row=0, column=0)
-        self.timer.grid(row=1, column=0)
-        self.tracker.grid(row=2, column=0)
-        self.controller.grid(row=3, column=0)
+        self.after(100, self.update)
 
-        self.after(100, self.update_app)
+    def countdown(self):
+        self.timer.countdown()
+        self.after_id = self.after(1000, self.countdown)
 
     def setup(self):
         self.title('Pomodoro Timer')
@@ -156,20 +204,16 @@ class App(tk.Tk):
         self.rowconfigure(2, weight=1)
         self.rowconfigure(3, weight=2)
     
-    # def create_warning(self, opened):
-    #     if not opened:
-    #         window = tk.Toplevel(self)
-
-    def update_app(self):
-        timer_option = self.option.get_selected()
-        started = self.controller.get_started()
-        if timer_option == 'Pomodoro':
-            self.timer.set_time(25*60000)
-        elif timer_option == 'Short Break':
-            self.timer.set_time(5*60000)
-        elif timer_option == 'Long Break':
-            self.timer.set_time(15*60000)
-        self.after(100, self.update_app)
+    def create_widgets(self):
+        self.option = OptionFrame(self)
+        self.timer = TimerFrame(self)
+        self.info = InfoFrame(self)
+        self.controller = ControllerFrame(self)
+        
+        self.option.grid(row=0, column=0)
+        self.timer.grid(row=1, column=0)
+        self.info.grid(row=2, column=0)
+        self.controller.grid(row=3, column=0)
 
 
 def main():
